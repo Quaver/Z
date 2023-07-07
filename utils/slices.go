2c16be924784c4551e8b230c00ae1a9eed38f058
Swan <me@swan.moe>
2023-07-07 08:41:06 -0400

    Add slice includes generic function & add comments

--- utils/slices.go
+++ utils/slices.go
@@ -1,5 +1,6 @@
 package utils
 
+// Filter Filters a slice based on a given condition
 func Filter[T any](slice []T, f func(T) bool) []T {
 	var n = make([]T, 0)
 
@@ -10,4 +11,15 @@ func Filter[T any](slice []T, f func(T) bool) []T {
 	}
 
 	return n
+}
+
+// Includes Returns if a slice contains an element
+func Includes[T comparable](slice []T, val T) bool {
+	for _, x := range slice {
+		if x == val {
+			return true
+		}
+	}
+
+	return false
 }
