72c09311e85c4cc96ace7058bd8c8131cf2b98ab
Swan <me@swan.moe>
2023-07-14 10:31:31 -0400

    Add comments to utils.mutex wrapper

--- utils/mutex.go
+++ utils/mutex.go
@@ -1,7 +1,11 @@
 package utils
 
-import "sync"
+import (
+	"sync"
+)
 
+// Mutex A wrapper around sync.Mutex which eliminates the need for constantly calling lock/unlock.
+// Very useful to prevent deadlocks, as only top-level functions should be calling RunLocked()
 type Mutex struct {
 	Mutex *sync.Mutex
 }
@@ -10,6 +14,7 @@ func NewMutex() *Mutex {
 	return &Mutex{Mutex: &sync.Mutex{}}
 }
 
+// RunLocked Runs a function in a locked context
 func (m *Mutex) RunLocked(f func()) {
 	m.Mutex.Lock()
 	defer m.Mutex.Unlock()
