a0e09e6f5fd8874618173c182158222525e54428
Swan <me@swan.moe>
2023-07-25 09:37:04 -0400

    Cleanup TestAddSpectator

--- spectator/spectator_test.go
+++ spectator/spectator_test.go
@@ -18,23 +18,21 @@ func TestAddSpectator(t *testing.T) {
 	db.InitializeRedis()
 	chat.Initialize()
 
-	user1 := sessions.NewUser(nil, &db.User{
-		Id:       1,
-		SteamId:  "1",
-		Username: "User #1",
-	})
-
+	user1 := sessions.NewUser(nil, &db.User{Id: 1, SteamId: "1", Username: "User #1"})
 	InitializeUser(user1)
 
-	user2 := sessions.NewUser(nil, &db.User{
-		Id:       2,
-		SteamId:  "2",
-		Username: "User #2",
-	})
-
+	user2 := sessions.NewUser(nil, &db.User{Id: 2, SteamId: "2", Username: "User #2"})
 	InitializeUser(user2)
 
 	GetUser(user1).AddSpectator(GetUser(user2))
+
+	if len(GetUser(user1).GetSpectators()) != 1 {
+		t.Fatal("Expected user 1 to have 1 spectator")
+	}
+
+	if len(GetUser(user2).GetSpectating()) != 1 {
+		t.Fatal("Expected user 2 to be spectating 1 person")
+	}
 
 	channel := chat.GetSpectatorChannel(user1.Info.Id)
 
@@ -44,14 +42,6 @@ func TestAddSpectator(t *testing.T) {
 
 	if len(channel.Participants) != 2 {
 		t.Fatal("Expected 2 participants in the channel")
-	}
-
-	if len(GetUser(user1).GetSpectators()) != 1 {
-		t.Fatal("Expected user 1 to have 1 spectator")
-	}
-
-	if len(GetUser(user2).GetSpectating()) != 1 {
-		t.Fatal("Expected user 2 to be spectating 1 person")
 	}
 
 	UninitializeUser(user1)
