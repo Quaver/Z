7fbe5a271b5d5847dc9a21acfbd3fcc7d575a665
Swan <me@swan.moe>
2024-07-21 16:06:32 -0400

    Reuse resty client

--- handlers/login.go
+++ handlers/login.go
@@ -38,6 +38,10 @@ type LoginData struct {
 	IsReconnection bool `json:"is_reconnection"`
 }
 
+var (
+	restyClient = resty.New()
+)
+
 // HandleLogin Handles the login of a client
 func HandleLogin(conn net.Conn, r *http.Request) error {
 	data, err := parseLoginData(r)
@@ -174,7 +178,7 @@ func authenticateSteamTicket(data *LoginData) error {
 		return nil
 	}
 
-	resp, err := resty.New().R().
+	resp, err := restyClient.R().
 		SetQueryParams(map[string]string{
 			"key":    config.Instance.Steam.PublisherKey,
 			"appid":  strconv.Itoa(config.Instance.Steam.AppId),
